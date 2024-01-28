a13fe1769db799d6e27e4cc011a2ae6f76d0e9d8
soochangyang <154221309+soochangyang@users.noreply.github.com>
2024-01-29 00:24:08 +1300

    Update ArtistRepository.java
    add findTopList

--- Music/src/main/java/nz/co/pfr/art/Music/entities/ArtistRepository.java
+++ Music/src/main/java/nz/co/pfr/art/Music/entities/ArtistRepository.java
@@ -1,8 +1,24 @@
 package nz.co.pfr.art.Music.entities;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
 import java.util.UUID;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 public interface ArtistRepository extends JpaRepository<Artist, UUID> {
+	@Query(value = "SELECT E.NAME\r\n"
+			+ "FROM (\r\n"
+			+ "    SELECT B.ARTISTID,  SUM(A.TRACK_CNT) TOT_TRACT_CNT\r\n"
+			+ "    FROM (\r\n"
+			+ "         SELECT CDID, COUNT(TRACKID) as TRACK_CNT\r\n"
+			+ "         FROM TRACK\r\n"
+			+ "         GROUP BY CDID\r\n"
+			+ "    ) A INNER JOIN CD AS B ON B.CDID = A.CDID\r\n"
+			+ "    GROUP BY B.ARTISTID\r\n"
+			+ ") D \r\n"
+			+ "INNER JOIN ARTIST E ON E.ARTISTID = D.ARTISTID\r\n"
+			+ "ORDER BY TOT_TRACT_CNT DESC LIMIT :topn", nativeQuery = true)
+	List<String> findTopList(@Param("topn") Integer topn); 
 }
